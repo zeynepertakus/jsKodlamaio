9aa5d7fb8f847f5b0beb7cea8d7ebf085982c45e
zeynepertakus <you@example.com>
2021-11-22 22:55:48 +0300

    18 ekim ders sonu.1

---
+++ src/appp.js
@@ -0,0 +1,33 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel konut kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>konut kredisi</li>
+    <li>emlak konut kredisi</li>
+    <li>kamu konut kredisi</li>
+</ul> */}
+
+
+
